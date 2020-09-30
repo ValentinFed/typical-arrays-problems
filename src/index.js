a0b146bc4b210b0d041e9218ca38537137621ee7
Valentin <val.fediuk@gmail.com>
2020-09-30 10:46:31 +0200

    feat: all done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (typeof array !== 'undefined' && array.length > 0) {
+    return Math.min(...array);
+  }
+    return 0
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (typeof array !== 'undefined' && array.length > 0) {
+    return Math.max(...array);
+  }
+    return 0
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (typeof array !== 'undefined' && array.length > 0) {
+    return array.reduce((a,b) => a + b, 0) / array.length;
+  }
+    return 0
 }
